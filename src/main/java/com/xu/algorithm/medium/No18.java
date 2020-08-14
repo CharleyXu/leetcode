e849ff1911771ed756cdd543cf8255a57e06d1d2
CharleyXu <xucharlie1993@gmail.com>
2020-08-14 12:05:26 +0800

    feat(conflict): 解决依赖问题

--- src/main/java/com/xu/algorithm/medium/No18.java
+++ src/main/java/com/xu/algorithm/medium/No18.java
@@ -1,133 +1,66 @@
-<<<<<<< HEAD:src/main/java/com/xu/algorithm/medium/No18.java
 package com.xu.algorithm.medium;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import org.junit.Test;
 
 /**
  * @author charlie Created on 2018/2/5.
- *
+ * <p>
  * 4Sum
- *
+ * <p>
  * 给定一个n个整数的数组S，是否存在S中的元素a，b，c和d，使得a + b + c + d = target？在数组中找出所有唯一的四元组，给出目标的总和。
  */
 public class No18 {
 
-	public List<List<Integer>> fourSum(int[] num, int target) {
-		List<List<Integer>> list = new ArrayList<>();
-		if (num == null || num.length < 4) {
-			return list;
-		}
-		Arrays.sort(num);
-		int length = num.length;
-		int i, j, low, high, temp;
-		for (i = 0; i < length - 3; i++) {
-			for (j = i + 1; j < length - 2; j++) {
-				low = j + 1;
-				high = length - 1;
-				temp = target - num[i] - num[j];
-				while (low < high) {
-					if (temp < num[low] + num[high]) {
-						high--;
-						continue;
-					}
-					if (temp == num[low] + num[high]) {
-						List<Integer> integers = new ArrayList<>(
-								Arrays.asList(num[i], num[j], num[low], num[high]));
-						list.add(integers);
-					}
-					low++;
-					while (low < high && num[low] == num[low - 1]) {
-						low++;
-					}
-				}
-				while (num[j] == num[j + 1] && j < length - 2) {
-					j++;
-				}
-			}
-			while (num[i] == num[i + 1] && i < length - 3) {
-				i++;
-			}
-		}
-		return list;
-	}
+    public List<List<Integer>> fourSum(int[] num, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (num == null || num.length < 4) {
+            return list;
+        }
+        Arrays.sort(num);
+        int length = num.length;
+        int i, j, low, high, temp;
+        for (i = 0; i < length - 3; i++) {
+            for (j = i + 1; j < length - 2; j++) {
+                low = j + 1;
+                high = length - 1;
+                temp = target - num[i] - num[j];
+                while (low < high) {
+                    if (temp < num[low] + num[high]) {
+                        high--;
+                        continue;
+                    }
+                    if (temp == num[low] + num[high]) {
+                        List<Integer> integers = new ArrayList<>(
+                                Arrays.asList(num[i], num[j], num[low], num[high]));
+                        list.add(integers);
+                    }
+                    low++;
+                    while (low < high && num[low] == num[low - 1]) {
+                        low++;
+                    }
+                }
+                while (num[j] == num[j + 1] && j < length - 2) {
+                    j++;
+                }
+            }
+            while (num[i] == num[i + 1] && i < length - 3) {
+                i++;
+            }
+        }
+        return list;
+    }
 
-	@Test
-	public void test() {
-		int[] arr = {1, 0, -1, 0, -2, 2};
-		int[] arr2 = {-3, -3, 1, 0, -1, 0, 0, 0, 0, 1, 1, -2, 2, 3, 3, 3, 3, 4};
-		int[] arr3 = {0, 4, -5, 2, -2, 4, 2, -1, 4};
-		int target = 0;
-		List<List<Integer>> list = fourSum(arr3, 12);
-		System.out.println(list);
-	}
+    @Test
+    public void test() {
+        int[] arr = {1, 0, -1, 0, -2, 2};
+        int[] arr2 = {-3, -3, 1, 0, -1, 0, 0, 0, 0, 1, 1, -2, 2, 3, 3, 3, 3, 4};
+        int[] arr3 = {0, 4, -5, 2, -2, 4, 2, -1, 4};
+        int target = 0;
+        List<List<Integer>> list = fourSum(arr3, 12);
+        System.out.println(list);
+    }
 }
-=======
-package com.xu.leetcode.medium;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import org.junit.Test;
-
-/**
- * @author charlie Created on 2018/2/5.
- *
- * 4Sum
- *
- * 给定一个n个整数的数组S，是否存在S中的元素a，b，c和d，使得a + b + c + d = target？在数组中找出所有唯一的四元组，给出目标的总和。
- */
-public class No18 {
-
-	public List<List<Integer>> fourSum(int[] num, int target) {
-		List<List<Integer>> list = new ArrayList<>();
-		if (num == null || num.length < 4) {
-			return list;
-		}
-		Arrays.sort(num);
-		int length = num.length;
-		int i, j, low, high, temp;
-		for (i = 0; i < length - 3; i++) {
-			for (j = i + 1; j < length - 2; j++) {
-				low = j + 1;
-				high = length - 1;
-				temp = target - num[i] - num[j];
-				while (low < high) {
-					if (temp < num[low] + num[high]) {
-						high--;
-						continue;
-					}
-					if (temp == num[low] + num[high]) {
-						List<Integer> integers = new ArrayList<>(
-								Arrays.asList(num[i], num[j], num[low], num[high]));
-						list.add(integers);
-					}
-					low++;
-					while (low < high && num[low] == num[low - 1]) {
-						low++;
-					}
-				}
-				while (num[j] == num[j + 1] && j < length - 2) {
-					j++;
-				}
-			}
-			while (num[i] == num[i + 1] && i < length - 3) {
-				i++;
-			}
-		}
-		return list;
-	}
-
-	@Test
-	public void test() {
-//		int[] arr = {1, 0, -1, 0, -2, 2};
-//		int[] arr2 = {-3, -3, 1, 0, -1, 0, 0, 0, 0, 1, 1, -2, 2, 3, 3, 3, 3, 4};
-		int[] arr3 = {0, 4, -5, 2, -2, 4, 2, -1, 4};
-		int target = 12;
-		List<List<Integer>> list = fourSum(arr3, target);
-		System.out.println(list);
-	}
-}
->>>>>>> 45881e9358f9bb61c0f9a28aa4aace01dbd96342:src/main/java/com/xu/leetcode/medium/No18.java
--- src/main/java/com/xu/algorithm/other/PrintMatrix.java
+++ src/main/java/com/xu/algorithm/other/PrintMatrix.java
@@ -1,133 +1,57 @@
-<<<<<<< HEAD:src/main/java/com/xu/algorithm/other/PrintMatrix.java
 package com.xu.algorithm.other;
 
 import org.junit.Test;
 
 /**
  * @author charlie Created on 2018/1/31. 回环矩阵
- *
+ * <p>
  * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字
- *
- *
+ * <p>
+ * <p>
  * 1		2		3		4
- *
+ * <p>
  * 5		6 	7		8
- *
+ * <p>
  * 9		10	11	12
- *
+ * <p>
  * 13		14	15	16
- *
+ * <p>
  * 依次打印出数字1、2、3、4、8、12、16、15、14、13、9、5、6、7、11、10
  */
 public class PrintMatrix {
 
-	@Test
-	public void test01() {
-		int[][] num = new int[20][20];
-		int length = 4;
-		int count = 1;
-		for (int i = 0; i < length; i++) {
-			for (int j = 0; j < length; j++) {
-				num[i][j] = count++;
-			}
-		}
-		output(num, 0, length - 1);
-	}
+    @Test
+    public void test01() {
+        int[][] num = new int[20][20];
+        int length = 4;
+        int count = 1;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                num[i][j] = count++;
+            }
+        }
+        output(num, 0, length - 1);
+    }
 
-	public void output(int[][] num, int start, int end) {
-		if (start > end || end < 1) {
-			return;
-		}
-		for (int i = start; i <= end; i++) {
-			System.out.print(num[start][i] + ",");
-		}
-		for (int i = start + 1; i <= end; i++) {
-			System.out.print(num[i][end] + ",");
-		}
-		for (int i = end - 1; i >= start; i--) {
-			System.out.print(num[end][i] + ",");
-		}
-		for (int i = end - 1; i > start; i--) {
-			System.out.print(num[i][start] + ",");
-		}
+    public void output(int[][] num, int start, int end) {
+        if (start > end || end < 1) {
+            return;
+        }
+        for (int i = start; i <= end; i++) {
+            System.out.print(num[start][i] + ",");
+        }
+        for (int i = start + 1; i <= end; i++) {
+            System.out.print(num[i][end] + ",");
+        }
+        for (int i = end - 1; i >= start; i--) {
+            System.out.print(num[end][i] + ",");
+        }
+        for (int i = end - 1; i > start; i--) {
+            System.out.print(num[i][start] + ",");
+        }
 
-		output(num, start + 1, end - 1);
+        output(num, start + 1, end - 1);
 
-	}
-
-}
-=======
-package com.xu.leetcode.other;
-
-import org.junit.Test;
-
-/**
- * @author charlie Created on 2018/1/31. 回环矩阵
- *
- * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字
- *
- *
- * 1		2		3		4
- *
- * 5		6 	7		8
- *
- * 9		10	11	12
- *
- * 13		14	15	16
- *
- * 依次打印出数字1、2、3、4、8、12、16、15、14、13、9、5、6、7、11、10
- */
-public class PrintMatrix {
-
-	@Test
-	public void test01() {
-		int[][] num = new int[20][20];
-		int length = 4;
-		int count = 1;
-		for (int i = 0; i < length; i++) {
-			for (int j = 0; j < length; j++) {
-				num[i][j] = count++;
-			}
-		}
-		output(num, 0, length - 1);
-	}
-
-	public void output(int[][] num, int start, int end) {
-		while (start < end) {
-			for (int i = start; i <= end; i++) {
-				System.out.print(num[start][i] + ",");
-			}
-			for (int i = start + 1; i <= end; i++) {
-				System.out.print(num[i][end] + ",");
-			}
-			for (int i = end - 1; i >= start; i--) {
-				System.out.print(num[end][i] + ",");
-			}
-			for (int i = end - 1; i > start; i--) {
-				System.out.print(num[i][start] + ",");
-			}
-			start = start + 1;
-			end = end - 1;
-		}
-//
-//		if (start > end || end < 1) {
-//			return;
-//		}
-//		for (int i = start; i <= end; i++) {
-//			System.out.print(num[start][i] + ",");
-//		}
-//		for (int i = start + 1; i <= end; i++) {
-//			System.out.print(num[i][end] + ",");
-//		}
-//		for (int i = end - 1; i >= start; i--) {
-//			System.out.print(num[end][i] + ",");
-//		}
-//		for (int i = end - 1; i > start; i--) {
-//			System.out.print(num[i][start] + ",");
-//		}
-//
-//		output(num, start + 1, end - 1);
-	}
+    }
 
 }
->>>>>>> 45881e9358f9bb61c0f9a28aa4aace01dbd96342:src/main/java/com/xu/leetcode/other/PrintMatrix.java
